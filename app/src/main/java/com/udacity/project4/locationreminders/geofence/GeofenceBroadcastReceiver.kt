a6d75c53ce5c977877a4f4b8f689572744056ddc
thib146 <thibaut@jumboprivacy.com>
2023-09-25 13:13:23 +0200

    add JobIntentService support for the geofencing/notification feature

--- app/src/main/java/com/udacity/project4/locationreminders/geofence/GeofenceBroadcastReceiver.kt
+++ app/src/main/java/com/udacity/project4/locationreminders/geofence/GeofenceBroadcastReceiver.kt
@@ -7,18 +7,8 @@ import android.util.Log
 import com.google.android.gms.location.Geofence
 import com.google.android.gms.location.GeofencingEvent
 import com.udacity.project4.R
-import com.udacity.project4.locationreminders.data.ReminderDataSource
-import com.udacity.project4.locationreminders.data.dto.ReminderDTO
-import com.udacity.project4.locationreminders.data.dto.Result
-import com.udacity.project4.locationreminders.reminderslist.ReminderDataItem
 import com.udacity.project4.locationreminders.savereminder.SaveReminderFragment.Companion.ACTION_GEOFENCE_EVENT
 import com.udacity.project4.utils.errorMessage
-import com.udacity.project4.utils.sendNotification
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import org.koin.core.component.KoinComponent
-import org.koin.core.component.inject
 
 /**
  * Triggered by the Geofence. Since we can have many Geofences at once, we pull the request
@@ -29,10 +19,8 @@ import org.koin.core.component.inject
  * To do that you can use https://developer.android.com/reference/android/support/v4/app/JobIntentService to do that.
  */
 
-class GeofenceBroadcastReceiver : BroadcastReceiver(), KoinComponent {
+class GeofenceBroadcastReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context, intent: Intent) {
-        val remindersLocalRepository: ReminderDataSource by inject()
-
         if (intent.action == ACTION_GEOFENCE_EVENT) {
             val geofencingEvent = GeofencingEvent.fromIntent(intent)
 
@@ -44,33 +32,7 @@ class GeofenceBroadcastReceiver : BroadcastReceiver(), KoinComponent {
 
             if (geofencingEvent != null && geofencingEvent.geofenceTransition == Geofence.GEOFENCE_TRANSITION_ENTER) {
                 Log.v(TAG, context.getString(R.string.geofence_entered))
-                val geofenceId = when {
-                    !geofencingEvent.triggeringGeofences.isNullOrEmpty() ->
-                        geofencingEvent.triggeringGeofences!![0].requestId
-                    else -> {
-                        Log.e(TAG, "No Geofence Trigger Found!")
-                        return
-                    }
-                }
-
-                CoroutineScope(Dispatchers.Default).launch {
-                    //get the reminder with the request id
-                    val result = remindersLocalRepository.getReminder(geofenceId)
-                    if (result is Result.Success<ReminderDTO>) {
-                        val reminderDTO = result.data
-                        //send a notification to the user with the reminder details
-                        sendNotification(
-                            context, ReminderDataItem(
-                                reminderDTO.title,
-                                reminderDTO.description,
-                                reminderDTO.location,
-                                reminderDTO.latitude,
-                                reminderDTO.longitude,
-                                reminderDTO.id
-                            )
-                        )
-                    }
-                }
+                GeofenceTransitionsJobIntentService.enqueueWork(context, intent)
             }
         }
     }
